db35809eb646d2509b5eae82dadc88e4ebc20b5f
Tom Gleitsmann <35902221+tmgleitsmann@users.noreply.github.com>
2019-11-21 18:41:41 -0600

    Updated UI

--- react-flask-nn/src/components/Homepage.js
+++ react-flask-nn/src/components/Homepage.js
@@ -24,7 +24,7 @@ class Homepage extends React.Component{
                             </div>
                         </div>
                         <div className="col-lg-8 mx-auto">
-                            <p className="text-faded mb-5">We do the heavy lifting for you. Just upload your images or videos and allow us to detect and track objects for you. *Videos in beta*</p>
+                            <p className="text-faded mb-5">We do the heavy lifting for you. Just upload your images or videos and allow us to detect and track objects for you.</p>
                         </div>
                     </div>
                 </header>
--- react-flask-nn/src/components/Runs.js
+++ react-flask-nn/src/components/Runs.js
@@ -19,7 +19,7 @@ class Runs extends React.Component{
                             <div className="col offset-lg-8 mx-auto text-center">
                                 <h2 className="text-white section-heading">Hang tight!</h2>
                                 <hr className="light my-4" />
-                                <p className="text-faded mb-4">If you don&#39;t see what you&#39;re looking for, there&#39;s a chance it might still be processing on the flask server. Processing times are dependent on the server so we may need a little time.</p>
+                                <p className="text-faded mb-4">If you don&#39;t see what you&#39;re looking for, there&#39;s a chance it might still be processing on the flask server. Processing times are dependent on the server and media type so we may need a little time.</p>
                                 <p className="text-faded mb-4">A loading icon should appear below if data is being processed.</p>
                                 {this.props.appstate.flaskprocessing ? <img src="../images/thumbnails/loading.gif" style={{"width":"96px", "height":"96px"}}/> : <div></div>}
                             </div>
